df70303196159032ba997af2bc78bb22bad37d1f
Jonas Tomaz <jonastomazalves@gmail.com>
2014-04-26 21:14:44 -0300

    Criando função de critério para um individuo mais evoluido

--- geneticAlgorithmn.py
+++ geneticAlgorithmn.py
@@ -1,3 +1,25 @@
-import Helpers.py
+import myHelpers
+from operator import add
+#
+#	ascii(B) = 66
+#	ascii(L) = 76
+#	ascii(R) = 82
+#	Media dos ASCIIs, 74.66, arredondando pra baixo 74
+# 	Somatorio de 1 + 2 + 3 + 4 + 5 = 15
+#	Numero magico eh 74 + 15 = 89
+#
+magicalNumber = 89
 
-print getIndividualsFromCSV
+def balance(individual, target=magicalNumber):
+	print individual
+	numbersSum = []
+	numbersSum.append(ord(individual[0]))
+	for x in xrange(1,len(individual)):
+		numbersSum.append(int(individual[x]))
+	totalSum = reduce(add, numbersSum)
+	return abs(target-totalSum)
+
+individuals = myHelpers.getIndividualsFromCSV("balance-scale.csv")
+
+
+print balance(individuals[4])
